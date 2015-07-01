df111de86cf8c3c34ed6b45a1da16e65e3775390
Cesar <cesarcardinali@hotmail.com>
2015-07-01 18:01:07 -0300

    Update keeping user cfg completly fixed and "tested"

--- src/main/Main.java
+++ src/main/Main.java
@@ -59,12 +59,12 @@ public class Main {
 		try {
 			writer = new BufferedWriter(new FileWriter(new File("update_log.txt")));
 			System.out.println("Updating ...");
-			writer.write("Updating ...");
+			writer.write("Updating ...\n");
 			
 			System.out.println("- Updating jar file");
-			writer.write("- Updating jar file");
+			writer.write("- Updating jar file\n");
 			try {
-				copyScript(new File("S:\\Rio_Itu\\Temporário\\Cesar\\BatteryTool.jar"), new File("BatteryTool.jar"));
+				copyScript(new File("S:\\Rio_Itu\\SAT\\BatteryTool.jar"), new File("BatteryTool.jar"));
 			} catch (IOException e1) {
 				System.out.println("Could not update the JAR file. Process cancelled");
 				writer.write("Could not update the JAR file. Process cancelled");
@@ -88,39 +88,45 @@ public class Main {
 			
 			
 			System.out.println("- Creating user cfg bkp");
+			writer.write("- Creating user cfg bkp\n");
 			copyScript(new File("Data\\cfgs\\user_cfg.xml"), new File("Data\\cfgs\\user_cfg.xml.bkp"));
+			copyScript(new File("Data\\cfgs\\pass.ini"), new File("Data\\cfgs\\pass.bkp"));
 			
 			
 			System.out.println("- Updating DATA folder");
-			writer.write("- Updating DATA folder");
+			writer.write("- Updating DATA folder\n");
 			try {
-				FileUtils.copyDirectory(new File("S:\\Rio_Itu\\Temporário\\Cesar\\Data"), new File("Data"));
+				FileUtils.copyDirectory(new File("S:\\Rio_Itu\\SAT\\Data"), new File("\\Data"));
 			} catch (IOException e) {
 				System.out.println("Could not update the DATA folder. Process cancelled");
-				writer.write("Could not update the DATA folder. Process cancelled");
+				writer.write("Could not update the DATA folder. Process cancelled\n");
 				e.printStackTrace();
 				writer.close();
 				System.exit(0);
 			}
 			
 			
-			System.out.println("- Restoring user cfg ");
+			System.out.println("- Restoring user cfg\n");
+			writer.write("- Restoring user cfg\n");
 			copyScript(new File("Data\\cfgs\\user_cfg.xml.bkp"), new File("Data\\cfgs\\user_cfg.xml"));
+			copyScript(new File("Data\\cfgs\\pass.bkp"), new File("Data\\cfgs\\pass.ini"));
 			System.out.println("- Deleting old file");
+			writer.write("- Deleting old file");
 			new File("Data\\cfgs\\user_cfg.xml.bkp").delete();
+			new File("Data\\cfgs\\pass.bkp").delete();
 			
 			
 			System.out.println("- Starting new application");
-			writer.write("- Starting new application");
+			writer.write("- Starting new application\n");
 			ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "cd " + new File("").getAbsolutePath() + " && java -jar \"Batterytool.jar\"");
 			
 	        System.out.println("Update successful");
-	        writer.write("Update successful");
+	        writer.write("Update successful\n");
 			try {
 				/*Process p =*/ builder.start();
 			} catch (IOException e) {
 				System.out.println("Could not restart the application");
-				writer.write("Could not restart the application");
+				writer.write("Could not restart the application\n");
 				e.printStackTrace();
 				writer.close();
 				System.exit(0);
